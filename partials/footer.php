e20857b7d47729fa2006eef428d975c2a8b8576c
MikkokuFlonne <feliciap@hotmail.fr>
2021-11-16 00:37:18 +0100

    update colors.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,48 @@
 
-</div>
-
 <footer>
+<div class="copyright">
+<p>Website created by MikkokuFlonne <i class="far fa-copyright"></i></p>
+</div>
+    <div class="reseaux_sociaux">
+                <h3>Mes réseaux sociaux</h3>
 
+                <div class="wrapper">
+                
+                    <div>
+                        <a href="https://twitter.com/MikkokuFlonne" target="_blank">
+                        <div class="icon twitter">
+                            <div class="tooltip">Twitter</div>
+                            <span><i class="fab fa-twitter"></i></span>
+                        </div>
+                        <p>@MikkokuFlonne</p>
+                        </a>
+                    </div>
+                    
+                    <div>
+                        <a href="https://github.com/MikkokuFlonne/" target="_blank">
+                            <div class="icon github">
+                                <div class="tooltip">Github</div>
+                                <span><i class="fab fa-github"></i></span>
+                            </div>
+                            <p>@MikkokuFlonne</p>
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="https://discord.com/users/94021342298378240/" target="_blank">
+                            <div class="icon discord">
+                                <div class="tooltip">Discord</div>
+                                <span><i class="fab fa-discord"></i></span>
+                            </div>
+                            <p>@Mikkoku#2380</p>
+                        </a>
+                    </div>
+
+                </div>
+    </div>
 </footer>
+
+</div>
 </body>
 
 </html>
--- partials/header.php
+++ partials/header.php
@@ -8,6 +8,7 @@ require_once "./vendor/autoload.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/bedc8cf35d.js" crossorigin="anonymous"></script>
     <title>Tools identification <?php
                                 if (isset($title)) {
                                     echo $title;
